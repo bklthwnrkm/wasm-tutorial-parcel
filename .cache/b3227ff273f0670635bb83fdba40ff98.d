78e45b5e4aaeca301af22eced9cf6027bf371a5c
hwatanabe <tufs.h.watanabe@gmail.com>
2023-05-18 10:17:03 +0900

    First and last commit!

---
+++ .cache/b3227ff273f0670635bb83fdba40ff98.d
@@ -0,0 +1,3 @@
+C:\Users\viz-job\Documents\wasm-tutorial-parcel\.cache\b3227ff273f0670635bb83fdba40ff98.d: C:\Users\viz-job\Documents\wasm-tutorial-parcel\src\lib.rs
+
+C:\Users\viz-job\Documents\wasm-tutorial-parcel\src\lib.rs:
---
+++ target/wasm32-unknown-unknown/release/deps/wasm_dev_book_parcel.d
@@ -0,0 +1,5 @@
+C:\Users\viz-job\Documents\wasm-tutorial-parcel\target\wasm32-unknown-unknown\release\deps\wasm_dev_book_parcel.wasm: src\lib.rs
+
+C:\Users\viz-job\Documents\wasm-tutorial-parcel\target\wasm32-unknown-unknown\release\deps\wasm_dev_book_parcel.d: src\lib.rs
+
+src\lib.rs:
